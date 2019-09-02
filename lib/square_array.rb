8a949dd5caacfbee0ae0cf4e9c4e6cd492e21d58
Gabriel Hafferty <gabe.hafferty.business@gmail.com>
2019-09-02 23:51:27 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,3 +1,7 @@
-def square_array(array)
-  # your code here
-end
+def square_array
+counter = 0
+  square_array = []
+  while array[counter] do
+    square_array.push(array[counter]**2)
+    counter += 1
+  end
